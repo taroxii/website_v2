c0abc09363d1d2309487c58b2c907abf570a96a2
shishong <monkey.d.chong@gmail.com>
2018-08-27 17:19:25 +0700

    updated

--- src/Pages/home/welcome.js
+++ src/Pages/home/welcome.js
@@ -10,8 +10,29 @@ export default class Welcome extends Component {
 
 
         this.state = {
-            title: 'Hello World',
-          };
+            title: '',
+
+            items: [
+                { link: '', caption: '', image_url: '//s.isanook.com/jo/0/rp/rc/w128h128/ya0xac0m1w0/aHR0cDovL2ltZ2NhY2hlLmpvb3guY29tL211c2ljL2pvb3gvcGhvdG9fdGhfdGgvdG9wbGlzdF8zMDAvMy83L2RkZmIyMjAwYWIzM2E3MjJlZjFlMDA2NmVjODc1ZTM3LmpwZw==.jpg' }
+                , { link: '', caption: '', image_url: '//s.isanook.com/jo/0/rp/rc/w128h128/ya0xac0m1w0/aHR0cDovL2ltZ2NhY2hlLmpvb3guY29tL211c2ljL2pvb3gvcGhvdG9fdGhfdGgvdG9wbGlzdF8zMDAvZS8xLzRiOWY4OWY0ODA0Zjc5OTVlNjcxNmRlNzgwNmU2MGUxLmpwZw==.jpg' }
+
+                , { link: '', caption: '', image_url: '//s.isanook.com/jo/0/rp/rc/w128h128/ya0xac0m1w0/aHR0cDovL2ltZ2NhY2hlLmpvb3guY29tL211c2ljL2pvb3gvcGhvdG9fdGhfdGgvdG9wbGlzdF8zMDAvZS8xLzRiOWY4OWY0ODA0Zjc5OTVlNjcxNmRlNzgwNmU2MGUxLmpwZw==.jpg' }
+
+                , { link: '', caption: '', image_url: '//s.isanook.com/jo/0/rp/rc/w128h128/ya0xac0m1w0/aHR0cDovL2ltZ2NhY2hlLmpvb3guY29tL211c2ljL2pvb3gvcGhvdG9fdGhfdGgvdG9wbGlzdF8zMDAvZS8xLzRiOWY4OWY0ODA0Zjc5OTVlNjcxNmRlNzgwNmU2MGUxLmpwZw==.jpg' }
+
+                , { link: '', caption: '', image_url: '//s.isanook.com/jo/0/rp/rc/w128h128/ya0xac0m1w0/aHR0cDovL2ltZ2NhY2hlLmpvb3guY29tL211c2ljL2pvb3gvcGhvdG9fdGhfdGgvdG9wbGlzdF8zMDAvZS8xLzRiOWY4OWY0ODA0Zjc5OTVlNjcxNmRlNzgwNmU2MGUxLmpwZw==.jpg' }
+
+                , { link: '', caption: '', image_url: '//s.isanook.com/jo/0/rp/rc/w128h128/ya0xac0m1w0/aHR0cDovL2ltZ2NhY2hlLmpvb3guY29tL211c2ljL2pvb3gvcGhvdG9fdGhfdGgvdG9wbGlzdF8zMDAvZS8xLzRiOWY4OWY0ODA0Zjc5OTVlNjcxNmRlNzgwNmU2MGUxLmpwZw==.jpg' }
+
+                , { link: '', caption: '', image_url: '//s.isanook.com/jo/0/rp/rc/w128h128/ya0xac0m1w0/aHR0cDovL2ltZ2NhY2hlLmpvb3guY29tL211c2ljL2pvb3gvcGhvdG9fdGhfdGgvdG9wbGlzdF8zMDAvZS8xLzRiOWY4OWY0ODA0Zjc5OTVlNjcxNmRlNzgwNmU2MGUxLmpwZw==.jpg' }
+
+                , { link: '', caption: '', image_url: '//s.isanook.com/jo/0/rp/rc/w128h128/ya0xac0m1w0/aHR0cDovL2ltZ2NhY2hlLmpvb3guY29tL211c2ljL2pvb3gvcGhvdG9fdGhfdGgvdG9wbGlzdF8zMDAvZS8xLzRiOWY4OWY0ODA0Zjc5OTVlNjcxNmRlNzgwNmU2MGUxLmpwZw==.jpg' }
+
+                , { link: '', caption: '', image_url: '//s.isanook.com/jo/0/rp/rc/w128h128/ya0xac0m1w0/aHR0cDovL2ltZ2NhY2hlLmpvb3guY29tL211c2ljL2pvb3gvcGhvdG9fdGhfdGgvdG9wbGlzdF8zMDAvZS8xLzRiOWY4OWY0ODA0Zjc5OTVlNjcxNmRlNzgwNmU2MGUxLmpwZw==.jpg' }
+
+                , { link: '', caption: '', image_url: '//s.isanook.com/jo/0/rp/rc/w128h128/ya0xac0m1w0/aHR0cDovL2ltZ2NhY2hlLmpvb3guY29tL211c2ljL2pvb3gvcGhvdG9fdGhfdGgvdG9wbGlzdF8zMDAvZS8xLzRiOWY4OWY0ODA0Zjc5OTVlNjcxNmRlNzgwNmU2MGUxLmpwZw==.jpg' }
+            ]
+        };
     }
 
     render() {
@@ -24,6 +45,7 @@ export default class Welcome extends Component {
 
                 <div  className="container">
                     
+                    
                     <div class="row two-columns home-main clearfix">
 
                         <div class="col primary-content">
@@ -33,7 +55,18 @@ export default class Welcome extends Component {
                                     <h1>ทัวร์แนะนำ</h1>
                                 </div>
 
-                                <div id="gr"></div>
+                                <div className="row">
+                                {this.state.items.map(value=>{
+                                    return(
+                                        <div className="items col col-md-2">
+                                            <div className="MediaThumbnail PostThumbnail">
+                                                <Link className="postImage pic" to={value.link}><img src={value.image_url} alt={value.caption} /></Link>
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                                    
+                                </div>
                             </div>
 
 
