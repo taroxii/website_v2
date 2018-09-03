f0b6a677d1716439bc2c4734c1daf33770e8fcbf
chayakornwc <whitecat.chayakorn@gmail.com>
2018-09-03 12:26:27 +0700

    :constructionworker: :lipstick: :hammer: bundles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Switch, BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Switch, BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
-import Child from './Child'
+
 import Home from "./Pages/home";
 //import Tour from "./Components/tour";
 import Packages from "./Components/packages";
@@ -12,13 +11,13 @@ import Packages from "./Components/packages";
 // set config Page 
 import pageLogo from "./dist/images/tourdd.f313fe7f.jpg";
 
-import {createBrowserHistory} from "history";
+import { createBrowserHistory } from "history";
 // 
 var hist = createBrowserHistory();
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.handleChange = this.handleChange.bind(this);
@@ -27,21 +26,20 @@ class App extends Component {
       site: {
         name: 'TourDD',
       },
-      title: 'Hello World',
       menu: [
-          {key: 'home',         name: 'หน้าแรก',             link: '/'}
-        , {key: 'packages',     name: 'แพ็คเกจทัวร์',     link: '/packages', }
+        { key: 'home', name: 'หน้าแรก', link: '/' }
+        , { key: 'packages', name: 'แพ็คเกจทัวร์', link: '/packages', }
         // , {key: 'recommend',    name: 'ทัวร์แนะนำ',         link: '/recommend'}
-        , {key: 'private-tour', name: 'จัดกรุ๊ปทัวร์ส่วนตัว',     link: '/private-tour',}
-        , {key: 'about-us',     name: 'เกี่ยวกับเรา',     link: '/about-us',}
-        , {key: 'blog',         name: 'บทความท่องเที่ยว',     link: '/blog',}
+        , { key: 'private-tour', name: 'จัดกรุ๊ปทัวร์ส่วนตัว', link: '/private-tour', }
+        , { key: 'about-us', name: 'เกี่ยวกับเรา', link: '/about-us', }
+        , { key: 'blog', name: 'บทความท่องเที่ยว', link: '/blog', }
       ],
 
       social: [
-          {key: 'facebook',         name: 'Facebook',             link: '/'}
-        , {key: 'line',         name: 'Line',             link: '/'}
-        , {key: 'Twitter',         name: 'Twitter',             link: '/'}
-        , {key: 'Instagram',         name: 'Instagram',             link: '/'}
+        { key: 'facebook', name: 'Facebook', link: '/' }
+        , { key: 'line', name: 'Line', link: '/' }
+        , { key: 'Twitter', name: 'Twitter', link: '/' }
+        , { key: 'Instagram', name: 'Instagram', link: '/' }
       ]
 
       , footer: {
@@ -49,26 +47,26 @@ class App extends Component {
           {
             title: 'Support',
             items: [
-                {id: '',         name: 'คำถามที่พบบ่อย',             link: '/faq'}
-              , {id: '',         name: 'ติดต่อเรา',             link: '/'}
-              , {id: '',         name: 'แนะนำ ติ-ชม',             link: '/'}
-              , {id: '',         name: 'ดาวน์โหลด',             link: '/'}
+              { id: '', name: 'คำถามที่พบบ่อย', link: '/faq' }
+              , { id: '', name: 'ติดต่อเรา', link: '/' }
+              , { id: '', name: 'แนะนำ ติ-ชม', link: '/' }
+              , { id: '', name: 'ดาวน์โหลด', link: '/' }
             ]
           }
-      ]
-    }
+        ]
+      }
 
     };
   }
- 
-  handleChange = (evt)=>{
-    this.setState ({
+
+  handleChange = (evt) => {
+    this.setState({
       name: evt.target.value
     })
   }
 
   render() {
-    // console.log( this.props.constructor );
+
     return (
       <Router>
 
@@ -83,7 +81,7 @@ class App extends Component {
                 <a href="/ms/en_TH/privacy_policy/privacy_policy.html">Privacy policy</a> <span class="pipeSeparator">|</span>
                 <a href="/ms/en_TH/privacy_policy/cookie_policy.html">Read more about cookies</a>
               </div>
-              
+
             </div>
           </div>
 
@@ -93,7 +91,7 @@ class App extends Component {
                 <div className="col">
                   <div id="help"><div className="title">Help</div></div>
                 </div>
-                
+
                 <div className="col-md-auto">
 
                   <div className="row">
@@ -119,35 +117,35 @@ class App extends Component {
           {/* header */}
           <header id="header" className="">
             <div className="container"><div className="row">
-                  <div className="col-md-auto"><h1 id="pageLogo" className="logo"><img src={pageLogo} /></h1></div>
+              <div className="col-md-auto"><h1 id="pageLogo" className="logo"><img src={pageLogo} /></h1></div>
 
-                  <nav className="col-md-auto" id="pageNav"><ul className="row nav">
-                  {this.state.menu.map(value=>{
-                    return(
-                      <li key={value.name} className="col"><Link className="link" to={value.link}>{value.name}</Link></li>
-                    )
-                  })}
-                  </ul></nav>
+              <nav className="col-md-auto" id="pageNav"><ul className="row nav">
+                {this.state.menu.map(value => {
+                  return (
+                    <li key={value.name} className="col"><Link className="link" to={value.link}>{value.name}</Link></li>
+                  )
+                })}
+              </ul></nav>
 
-                  <div id="pageSearch" className="col">
-                    <form>
-                      <div className="input-field">
-                        <input type="text" id="search-input" autoComplete="off" />
-                        <label htmlFor="search-input">ค้นหาทัวร์</label>
-                      </div>
-                     
-                    </form>
+              <div id="pageSearch" className="col">
+                <form>
+                  <div className="input-field">
+                    <input type="text" id="search-input" autoComplete="off" />
+                    <label htmlFor="search-input">ค้นหาทัวร์</label>
                   </div>
+
+                </form>
+              </div>
             </div></div>
           </header>
           {/* end: header */}
 
-          {/* main */}  
+          {/* main */}
           <main id="main">
             <Switch>
-                <Route exact path="/" name="home" component={Home} />
-                <Route  path="/packages" name="packages" component={Packages} />
-              </Switch>
+              <Route exact path="/" name="home" component={Home} />
+              <Route path="/packages" name="packages" component={Packages} />
+            </Switch>
           </main>
           {/* end: main */}
 
@@ -155,37 +153,37 @@ class App extends Component {
           <footer id="footer">
             <div className="container">
               <div className="row">
-                  <div className="col col-md-3">
-                    <div className="footer-logo">
-                      <img src={pageLogo} />
-                    </div>
-                    <div className="footer-social">
-                      
-                      <span className="title">ติดตามเราได้ที่:</span>
-                      
-                      {this.state.social.map((value, index)=>{
-                        return(
-                          <Link key={value.key} className={`link ${value.key}`} target="_blank" to={value.link}><span className="textreplacement">{value.name}</span></Link>
-                        )
-                      })}
-
-                    </div>
-                    <div className="footer-copyright">Copyright © 2011 - 2018 BKK Software. All Rights Reserved</div>
+                <div className="col col-md-3">
+                  <div className="footer-logo">
+                    <img src={pageLogo} />
                   </div>
-                  <div className="col col-md-3">
+                  <div className="footer-social">
 
-                      <h3>{this.state.footer.nav && this.state.footer.nav[0].title}</h3>
-                      <ul className="footerNav">
-                      {this.state.footer.nav[0].items.map((value, index)=>{
-                        return(
-                          <li key={index} className=""><Link to={value.link}>{value.name}</Link></li>
-                        )
-                      })}
-                        
-                      </ul>
+                    <span className="title">ติดตามเราได้ที่:</span>
+
+                    {this.state.social.map((value, index) => {
+                      return (
+                        <Link key={value.key} className={`link ${value.key}`} target="_blank" to={value.link}><span className="textreplacement">{value.name}</span></Link>
+                      )
+                    })}
+
                   </div>
-                  <div className="col col-md-3"></div>
-                  <div className="col col-md-3"></div>
+                  <div className="footer-copyright">Copyright © 2011 - 2018 BKK Software. All Rights Reserved</div>
+                </div>
+                <div className="col col-md-3">
+
+                  <h3>{this.state.footer.nav && this.state.footer.nav[0].title}</h3>
+                  <ul className="footerNav">
+                    {this.state.footer.nav[0].items.map((value, index) => {
+                      return (
+                        <li key={index} className=""><Link to={value.link}>{value.name}</Link></li>
+                      )
+                    })}
+
+                  </ul>
+                </div>
+                <div className="col col-md-3"></div>
+                <div className="col col-md-3"></div>
               </div>
             </div>
           </footer>
---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const extractCSS = new ExtractTextPlugin('[name].fonts.css');
+const extractSCSS = new ExtractTextPlugin('[name].styles.css');
+
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const SRC_DIR = path.resolve(__dirname, 'src');
+
+
+module.exports = (env = {}) => {
+  return {
+    entry: {
+      index: [SRC_DIR + '/index.js']
+    },
+    output: {
+      path: BUILD_DIR,
+      filename: '[name].bundle.js'
+    },
+    watch: true,
+    devtool: env.prod ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: BUILD_DIR,
+      port: 9001,
+      compress: true,
+      hot: true,
+      open: true
+    },
+    optimization: {
+      minimizer: [
+        // we specify a custom UglifyJsPlugin here to get source maps in production
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          uglifyOptions: {
+            compress: false,
+            ecma: 8,
+            mangle: true
+          },
+          sourceMap: true
+        })
+      ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              presets: ['es2017', 'stage-3', 'react']
+            }
+          }
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader'
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          use: [
+            {
+              // loader: 'url-loader'
+              loader: 'file-loader',
+              options: {
+                name: './img/[name].[hash].[ext]'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'file-loader',
+          options: {
+            name: './fonts/[name].[hash].[ext]'
+          }
+        }]
+    },
+    plugins: [
+      // Makes some environment variables available in index.html.
+      // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
+      // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+      // In development, this will be an empty string.
+     
+      new CleanWebpackPlugin(['build']),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+      extractCSS,
+      extractSCSS,
+       // Generates an `index.html` file with the <script> injected.
+      new HtmlWebpackPlugin(
+        {
+          inject: true,
+          template: './public/index.html'
+        }
+      ),
+      new CopyWebpackPlugin([
+        { from: './public/dist', to: 'dist' }
+      ],
+        { copyUnmodified: false }
+      )
+    ]
+  }
+};
